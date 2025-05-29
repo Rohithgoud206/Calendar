189533bd0a8c370e8f55cffed7db0da06156e6b0
Rohith Goud <rohithgoud206@gmail.com>
2025-05-29 19:13:29 +0530

    Update backgroundImages.ts

--- src/data/backgroundImages.ts
+++ src/data/backgroundImages.ts
@@ -1,17 +1,11 @@
 import { BackgroundImageMap } from '../types';
 
-// Monthly background images
 export const backgroundImages: BackgroundImageMap = {
-  0: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg', // January - Winter scene with snow
-  1: 'https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg', // February - Winter forest path
-  2: 'https://images.pexels.com/photos/1122868/pexels-photo-1122868.jpeg', // March - Spring flowers
-  3: 'https://images.pexels.com/photos/1891234/pexels-photo-1891234.jpeg', // April - Cherry blossoms
-  4: 'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg', // May - Spring meadow
-  5: 'https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg', // June - Beach scene
-  6: 'https://images.pexels.com/photos/46253/mt-fuji-sea-of-clouds-sunrise-46253.jpeg', // July - Summer mountain
-  7: 'https://images.pexels.com/photos/1237119/pexels-photo-1237119.jpeg', // August - Sunflower field
-  8: 'https://images.pexels.com/photos/1448055/pexels-photo-1448055.jpeg', // September - Fall colors
-  9: 'https://images.pexels.com/photos/1438761/pexels-photo-1438761.jpeg', // October - Autumn leaves
-  10: 'https://images.pexels.com/photos/2310713/pexels-photo-2310713.jpeg', // November - Late autumn scene
-  11: 'https://images.pexels.com/photos/1438761/pexels-photo-1438761.jpeg', // December - Snow scene
-};
+  0: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg',
+  1: 'https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg',
+  2: 'https://images.pexels.com/photos/1122868/pexels-photo-1122868.jpeg',
+  3: 'https://images.pexels.com/photos/1891234/pexels-photo-1891234.jpeg',
+  4: 'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg',
+  5: 'https://images.pexels.com/photos/46710/pexels-photo-46710.jpeg',
+  6: 'https://images.pexels.com/photos/46253/mt-fuji-sea-of-clouds-sunrise-46253.jpeg',
+  7: 'https://images.pexels.com/photos/1237119/pexels-photo-1237119.jpeg'
